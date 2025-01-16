b2a0d50c0159658f6cb8bf4029572231dc491008
Victor Galvão <galvaomercante@gmail.com>
2025-01-16 12:52:15 -0300

    position menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,3 +9,19 @@ function toggleLink() {
     const menu = document.querySelector('.btn-header'); 
     menu.classList.remove('active');  
 }
+
+    // Função para rolar até o topo da página
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'  // Faz a rolagem ser suave
+        });
+    }
+
+    // Adicionando evento de clique aos links para chamar a função de rolar até o topo
+    document.querySelectorAll('.btn-header link').forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Impede o comportamento padrão do link
+            scrollToTop();  // Chama a função de rolar até o topo
+        });
+    });
