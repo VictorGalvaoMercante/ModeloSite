9cb9216292fee0eb33b2d8be5e3d70a8ffdb4c3a
Victor Galvão <galvaomercante@gmail.com>
2025-01-16 11:01:53 -0300

    ajustes menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,12 @@
-// Função para alternar o menu
+
 function toggleMenu() {
     const menu = document.querySelector('.btn-header');
-    menu.classList.toggle('active');
+    menu.classList.toggle('active'); 
 }
 
-// Adiciona uma classe 'scrolled' quando o usuário rolar a página
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
+
+function toggleLink() {
+    const menu = document.querySelector('.btn-header'); 
+    menu.classList.remove('active');  
+}
+
