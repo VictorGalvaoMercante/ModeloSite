8e2f8400903d06191dc53d05ea47b8f85cb462af
Victor Galvão <galvaomercante@gmail.com>
2025-01-17 12:03:28 -0300

    JanelaModal

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,3 +18,23 @@ function scrollToTop() {
         behavior: 'smooth'  
     });
 }
+
+
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".close-btn");
+
+
+setTimeout(() => {
+    modal.style.display = "flex";
+}, 5000);
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
